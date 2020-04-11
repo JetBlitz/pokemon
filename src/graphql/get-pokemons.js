ab9e2a06a3b38c8d5c7f050e6583efe4535e2f15
Eivin Adlawan <eadlawa2@gmail.com>
2020-04-11 00:50:58 -0500

    Savepoint: adding PokemonsContainer into App.js, deleted extra files, added apollo

---
+++ src/graphql/get-pokemons.js
@@ -0,0 +1,29 @@
+import gql from "graphql-tag";
+
+export const GET_POKEMONS = gql`
+query pokemons($first: Int!) {
+  pokemons(first: $first) {
+    id
+    name
+    image
+    maxHP
+    maxCP
+    attacks {
+      fast {
+        name
+        type
+        damage
+      }
+      special {
+        name
+        type
+        damage
+      }
+    }
+    evolutions {
+      name
+    }
+  }
+  }
+}
+`
